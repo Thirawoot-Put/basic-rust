c0da9e4fdebeb6ce1d74b463e6deced52404de1a
Thirawoot-P <thirawoot.put@gmail.com>
2024-09-03 22:57:41 +0700

    comment

--- hello-world/hello.rs
+++ hello-world/hello.rs
@@ -1,4 +1,11 @@
 fn main() {
+    // this is single line comment in rust!!!
+    /*
+     * this is multiple line comment in rust!!!
+     *
+     * */
+    /// this is doc comment => more information: https://doc.rust-lang.org/rust-by-example/meta/doc.html
+
     println!("Hello World!");
     println!("I'm a Rustacean!");
 }
