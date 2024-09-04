f1c87ae9855bf0a5359f332482ddcfade42fbc17
Thirawoot-P <thirawoot.put@gmail.com>
2024-09-05 05:42:00 +0700

    print formatted

--- hello-world/hello.rs
+++ hello-world/hello.rs
@@ -4,8 +4,34 @@ fn main() {
      * this is multiple line comment in rust!!!
      *
      * */
-    /// this is doc comment => more information: https://doc.rust-lang.org/rust-by-example/meta/doc.html
 
     println!("Hello World!");
     println!("I'm a Rustacean!");
+
+    println!("{} days", 1);
+    println!(
+        "Hi, I'm {0}. I learining about {1}. {0} will be {1} developer.",
+        "Tle", "Rust"
+    );
+    println!("Base 10: {}", 1234);
+    println!("Base 2(binary): {:b}", 1234);
+    println!("Base 8(octal): {:o}", 1234);
+    println!("Base hexadecimal: {:x}", 1234);
+    println!("{num:>3}", num = 1);
+    println!("{num:0>3}", num = 1);
+    println!("{num:0<3}", num = 1);
+    println!("{num:>width$}", num = 1, width = 5);
+
+    // This will not compile because `Structure` not implement
+    // #[allow(dead_code)] // disable `dead_code` which warn against unused module
+    // struct Structure(i32);
+
+    // This will not compile because `Structure` does not implement
+    // fmt::Display.
+    // println!("This struct `{}` won't print...", Structure(3));
+    // TODO ^ Try uncommenting this line
+
+    let num: f64 = 4.0;
+    let width: usize = 5;
+    println!("{num:>width$}");
 }
